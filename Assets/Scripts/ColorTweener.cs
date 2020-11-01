75e9677bcedc76519936b1d6a36a178e86a9970f
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-11-01 16:12:30 +0100

    PingPongScaleTweener!

---
+++ Assets/Scripts/ColorTweener.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Color tweener.
+/// Interpolates the renderer's material.color of a GameObject by looping through the list of color steps defined in the inspector.
+/// Keeps a counter to the number of interpolation loops completed since the script started. See <see cref="Loops"/>.
+/// </summary>
+public class ColorTweener : BaseTweener
+{
+
+    #region Fields/Properties
+
+    [SerializeField]
+    [Tooltip("List of ordered color steps to interpolate each loop.")]
+    private List<Color> _steps;
+
+    // Renderer component in the GameObject.
+    private Renderer _myRenderer;
+
+    // Initial color of the object. Needed for the initial lerp.
+    private Color _initialColor;
+
+    #endregion
+
+    #region Lifecycle
+
+    private void Awake()
+    {
+        _myRenderer = GetComponent<Renderer>();
+        _initialColor = _myRenderer.material.color;
+    }
+
+    # endregion  
+
+    #region BaseTweener overrides
+
+    protected override int GetNumSteps() 
+    {
+        return _steps.Count;
+    }
+
+    protected override void DoInitialLerp(float stepRatio) 
+    {
+        _myRenderer.material.color = Color.Lerp(_initialColor, _steps[0], stepRatio);
+    }
+
+    protected override void DoLerp(int startIndex, int endIndex, float stepRatio)
+    {
+        _myRenderer.material.color = Color.Lerp(_steps[startIndex], _steps[endIndex], stepRatio);
+    }  
+
+    #endregion
+
+}
---
+++ Assets/Scripts/PositionTweener.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Position tweener.
+/// Interpolates the transform.position of a GameObject by looping through the list of position steps defined in the inspector.
+/// Keeps a counter to the number of interpolation loops completed since the script started. See <see cref="Loops"/>.
+/// </summary>
+public class PositionTweener : BaseTweener
+{
+
+    #region Fields/Properties
+
+    [SerializeField]
+    [Tooltip("List of ordered position steps to interpolate each loop.")]
+    private List<Vector3> _steps;
+
+    // Initial position of the object. Needed for the initial lerp.
+    private Vector3 _initialPosition;
+
+    #endregion
+
+    #region Lifecycle
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    # endregion  
+    
+
+    #region BaseTweener overrides
+
+    protected override int GetNumSteps() 
+    {
+        return _steps.Count;
+    }  
+
+    protected override void DoInitialLerp(float stepRatio) 
+    {
+        transform.position = Vector3.Lerp(_initialPosition, _steps[0], stepRatio);
+    }
+
+    protected override void DoLerp(int startIndex, int endIndex, float stepRatio)
+    { 
+         transform.position = Vector3.Lerp(_steps[startIndex], _steps[endIndex], stepRatio);
+    }  
+
+    #endregion
+
+}
